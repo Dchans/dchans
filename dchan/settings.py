dacfee43a03968c6a60acb7f7b2aada7fd3ff910
Dchans <135418178+Dchans@users.noreply.github.com>
2023-08-16 20:16:32 +0530

    Update settings.py

--- dchan/settings.py
+++ dchan/settings.py
@@ -79,10 +79,20 @@ WSGI_APPLICATION = 'dchan.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}"""
+DATABASES={
+    'default':{
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME':'dchans',
+        'USER':'devendran',
+        'PASSWORD':'dchan@9120',
+        'HOST':'localhost',
+        'PORT':'',
     }
 }
 REST_FRAMEWORK = {
